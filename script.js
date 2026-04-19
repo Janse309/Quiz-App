9440646f50996685b140c03bb2fe7bb2796a6411
janse <jansimonboecker40@gmail.com>
2026-04-19 13:36:20 +0200

    added json for questions and answers

---
+++ script.js
@@ -0,0 +1,76 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Was bedeutet die Abkürzung 'CSS'?",
+        "answer_1": "Creative Style Sheets",
+        "answer_2": "Cascading Style Sheets",
+        "answer_3": "Computer Style Sheets",
+        "answer_4": "Colorful Style Sheets",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Unternehmen entwickelte die Programmiersprache Java?",
+        "answer_1": "Microsoft",
+        "answer_2": "Apple",
+        "answer_3": "Sun Microsystems",
+        "answer_4": "Google",
+        "right_answer": 3
+    },
+    {
+        "question": "Welches HTML-Tag wird verwendet, um eine JavaScript-Datei einzubinden?",
+        "answer_1": "<script>",
+        "answer_2": "<javascript>",
+        "answer_3": "<js>",
+        "answer_4": "<link>",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie nennt man den Vorgang, Fehler in einem Programm zu finden und zu beheben?",
+        "answer_1": "Coding",
+        "answer_2": "Compiling",
+        "answer_3": "Executing",
+        "answer_4": "Debugging",
+        "right_answer": 4
+    },
+    {
+        "question": "Welches Symbol wird in JavaScript für den Vergleich von Wert und Typ (Strict Equality) verwendet?",
+        "answer_1": "=",
+        "answer_2": "==",
+        "answer_3": "===",
+        "answer_4": "!=",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist die Hauptfunktion einer 'Array.push()' Methode in JavaScript?",
+        "answer_1": "Das erste Element eines Arrays löschen",
+        "answer_2": "Ein Element am Ende des Arrays hinzufügen",
+        "answer_3": "Das Array alphabetisch sortieren",
+        "answer_4": "Die Länge des Arrays auf 0 setzen",
+        "right_answer": 2
+    }
+];
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = questions[0]['answer_1'];
+    document.getElementById('answer_2').innerHTML = questions[0]['answer_2'];
+    document.getElementById('answer_3').innerHTML = questions[0]['answer_3'];
+    document.getElementById('answer_4').innerHTML = questions[0]['answer_4'];
+}
